5f89b74439c24765997238c6259a80c9d31f70b3
Qbert <wxn.npc@gmail.com>
2025-04-21 05:47:35 -0600

    Remove box item

--- src/components/NasaArticle.jsx
+++ src/components/NasaArticle.jsx
@@ -5,25 +5,14 @@ const NasaArticle = ({ article }) => {
 
     return (
         <>
-            <Box
-                sx={{
-                    backgroundColor: "rgba(190, 80, 110, 0.14)",
-                    padding: 2,
-                    borderRadius: 2,
-                    backdropFilter: "blur(6px)",
-                    mt: 2,
-                }}
-            >
             <Typography variant="h3" gutterBottom color="#6C6A61">{article.title}</Typography>
             <Typography variant="body2" fontSize="1.5rem" color="#6C6A61">
-                    &nbsp;&nbsp;published at:&nbsp;{article.date} <br />&nbsp;&nbsp;by:&nbsp; {article.copyright?.trim() ? article.copyright : "NASA"}
-                    <br /><br />
+                &nbsp;&nbsp;published at:&nbsp;{article.date} <br />&nbsp;&nbsp;by:&nbsp; {article.copyright?.trim() ? article.copyright : "NASA"}
+                <br /><br />
             </Typography>
-                <Typography variant="body1" fontSize="1.7rem" color="#BDC3CB">
+            <Typography variant="body1" fontSize="1.7rem" color="#BDC3CB">
                 &nbsp;&nbsp;&nbsp;&nbsp;{article.explanation}
             </Typography>
-
-            </Box>
         </>
     );
 };
