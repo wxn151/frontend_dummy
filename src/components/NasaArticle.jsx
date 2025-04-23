bd95c61ef83021a41a69c5227dd9158847b47f9e
wxn151 <wxn.npc@gmail.com>
2025-04-23 12:59:01 -0600

    edit NasaArticle.jsx (font)
    adjust font size

--- src/components/NasaArticle.jsx
+++ src/components/NasaArticle.jsx
@@ -7,10 +7,10 @@ const NasaArticle = ({ article }) => {
         <>
             <Typography variant="h3" gutterBottom color="#6C6A61">{article.title}</Typography>
             <Typography variant="body2" fontSize="1.5rem" color="#6C6A61">
-                &nbsp;&nbsp;published at:&nbsp;{article.date} <br />&nbsp;&nbsp;by:&nbsp; {article.copyright?.trim() ? article.copyright : "NASA"}
+                &nbsp;&nbsp;published:&nbsp;{article.date} <br />&nbsp;&nbsp;by:&nbsp; {article.copyright?.trim() ? article.copyright : "NASA"}
                 <br /><br />
             </Typography>
-            <Typography variant="body1" fontSize="1.7rem" color="#BDC3CB">
+            <Typography variant="body1" fontSize="1.4rem" color="#BDC3CB">
                 &nbsp;&nbsp;&nbsp;&nbsp;{article.explanation}
             </Typography>
         </>
